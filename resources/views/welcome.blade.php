2fb4f9dfb641a4ed09f50bfeaf347ed9842f9c68
taufik0910 <taufik.cradle@gnail.com>
2017-11-26 15:19:10 +0700

    tes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Recipe box</title>
     
-    <link rel="stylesheet" type>
-    
+    <link rel="stylesheet" type="text/css" href="{{mix('css/app.css')}}">
+
+
+   
        
     </head>
     <body>
        <div id="root"></div>
     </body>
+    <<script type="text/javascript" src="{{mix('js/app.js')}}">
+        
+    </script>
 </html>
+  
